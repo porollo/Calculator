e4f22a22972e77a031ba92881d155c3df2174dbc
Spider <d.porollo@gmail.com>
2017-01-19 15:54:05 +0300

    Initial Commit

---
+++ Calculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Calculator
+//
+//  Created by Dmitriy Porollo on 19.01.17.
+//  Copyright © 2017 Dmitriy Porollo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dmitriy Porollo on 19.01.17.
+//  Copyright © 2017 Dmitriy Porollo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
